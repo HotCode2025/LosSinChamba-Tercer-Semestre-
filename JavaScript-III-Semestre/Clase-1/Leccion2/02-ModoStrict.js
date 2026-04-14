38300f000d3421e42ed787d8f22b730ef3ba5001
Lautaro Martinez <lautimartinezapooo@gmail.com>
2026-04-14 18:51:02 -0300

    Add files via upload

---
+++ JavaScript-III-Semestre/Clase-1/Leccion2/02-ModoStrict.js
@@ -0,0 +1,15 @@
+// Grupo: Los sin chamba
+ 
+"use strict";
+ 
+let x = 50;
+console.log(x);
+ 
+function miFuncion(){
+    "use strict";
+    let y = 50;
+    console.log(y); // 10
+}
+ 
+miFuncion();
+ 
