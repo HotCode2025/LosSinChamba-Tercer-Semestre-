83301d5b34224608228675af16c75dbf94e68a2d
Lautaro Martinez <lautimartinezapooo@gmail.com>
2026-04-13 23:14:31 -0300

    tarea de javascrip

---
+++ Programacion 2026/Javascrip/Leccion2/02-ModoStrict.js
@@ -0,0 +1,15 @@
+// Grupo: Los sin chamba
+ 
+"use strict";
+ 
+x = 10; // x is not defined
+console.log(x);
+ 
+function miFuncion(){
+    "use strict";
+    let y = 10;
+    console.log(y); // 10
+}
+ 
+miFuncion();
+ 
