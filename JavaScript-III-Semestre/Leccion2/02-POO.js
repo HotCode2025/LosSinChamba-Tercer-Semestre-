113fdc7e543fc6355c34944826aacb920e2ec97d
Lautaro Martinez <lautimartinezapooo@gmail.com>
2026-04-20 22:13:47 -0300

    clase 20 de abril

--- JavaScript-III-Semestre/Leccion2/02-POO.js
+++ JavaScript-III-Semestre/Leccion2/02-POO.js
@@ -22,6 +22,26 @@ class Gerente extends Empleado{
         return `Gerente: ${super.obtenerDetalles()} depto: ${this._departamento}`;
     }
 }
+function imprimir( tipo ){ //Recibe un variable
+    console.log( tipo.obtenerDetalles() ); //Según el tipo que le pasemos, será la información
+    if( tipo instanceof Gerente){
+        console.log('Es un objeto de tipo Gerente');
+        console.log( tipo._departamento ) //Existe en Gerente
+    }
+    else if( tipo instanceof Empleado){
+        console.log('Es de tipo Empleado');
+        console.log( tipo._departamento ) //No existe en Empleado
+    }
+    else if( tipo instanceof Object){ //El orden siempre es jerárquico
+        console.log('Es de tipo Object'); //Clase padre de todas las clases
+    }
+}
 
-let gerente1 = new Gerente("Los sin chamba", 47000, "Sistemas");
+let gerente1 = new Gerente("Carlos", 5000, "Sistemas");
 console.log(gerente1); //Objeto de la clase hija
+
+let empleado1 = new Empleado("Juan", 3000);
+console.log(empleado1); //Objeto de la clase padre
+
+imprimir( gerente1 );
+imprimir( empleado1 );
