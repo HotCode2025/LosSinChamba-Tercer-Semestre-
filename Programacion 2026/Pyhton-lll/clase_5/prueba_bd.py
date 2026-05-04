77b6b8130734168b52d5bc5ea92e6852a45fa3c5
Lautaro Martinez <lautimartinezapooo@gmail.com>
2026-05-04 20:40:19 -0300

    hice tarea de py en mi rama de la clase 5

---
+++ Programacion 2026/Pyhton-lll/clase_5/prueba_bd.py
@@ -0,0 +1,14 @@
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' # Placeholder
+            id_persona = input('Digite un número para el id_persona: ')
+            cursor.execute(sentencia, (id_persona,)) # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchone() # Recuperamos todos los registros que serán una lista
+            print(registros)
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
+
+# https://www.psycopg.org/docs/usage.html
